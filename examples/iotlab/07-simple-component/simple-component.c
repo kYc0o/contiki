176f522a8e757e61ee8a1271e938fc0b3c145174
Inti Gonzalez-Herrera <inti.glez@gmail.com>
2015-04-29 03:10:04 +0200

    removing useless process

--- examples/iotlab/07-simple-component/simple-component.c
+++ examples/iotlab/07-simple-component/simple-component.c
@@ -45,28 +45,4 @@ int updateHelloWorld(void* instance)
     return 0;
 }
 
-REGISTER_COMPONENT("HelloWorldComponent", helloWorld)
-
-/*
- * Prints "Hello World !", and echoes whatever arrives on the serial link
- */
-
-PROCESS(serial_echo, "Serial Echo");
-//AUTOSTART_PROCESSES(&serial_echo);
-
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(serial_echo, ev, data)
-{
-  PROCESS_BEGIN();
-
-  printf("Hello World !\n");
-  while(1) {
-    printf("> ");
-    PROCESS_YIELD();
-    if (ev == serial_line_event_message) {
-      printf("Echo cmd: '%s'\n", (char*)data);
-    }
-  }
-  PROCESS_END();
-}
-/*---------------------------------------------------------------------------*/
+REGISTER_COMPONENT(1, "HelloWorldComponent", &helloWorld)
