fcfc6503733ecfdff7ef6b41f063ef72e5fd7b1f
kYc0o <fco.ja.ac@gmail.com>
2015-04-30 19:37:52 +0200

    deserialization added

--- examples/kev-runtime/shell_group.c
+++ examples/kev-runtime/shell_group.c
@@ -3,6 +3,11 @@
 #include "json.h"
 #include "jsonparse.h"
 
+#include "ContainerRoot.h"
+#include "JSONModelLoader.h"
+#include "Visitor.h"
+#include "ModelCompare.h"
+
 #include "rtkev.h"
 #include "shell_group.h"
 
@@ -11,7 +16,11 @@ static void* newShellGroup(const char* name);
 static int startShellGroup(void* instance);
 static int stopShellGroup(void* instance);
 static int updateShellGroup(void* instance);
-static ContainerRoot *newModel;
+
+ContainerRoot *intiModel;
+int fd_read;
+uint32_t length;
+char *jsonModel;
 
 const GroupInterface ShellGroupInterface = {
 		.name = "ShellGroupType",
@@ -32,15 +41,12 @@ process_event_t NEW_MODEL_IN_JSON;
 PROCESS(shellGroupP, "shellGroupProcess");
 PROCESS_THREAD(shellGroupP, ev, data)
 {
-	int fd_read;
-	uint32_t length;
-	char *jsonModel;
 	static ShellGroup *instance;
 
 	PROCESS_BEGIN();
 
 	instance = (ShellGroup*)data;
-	
+
 	printf("Ok, here I have my instance %p\n", (struct ShellGroup*)data);
 
 	/* define new event */
@@ -50,7 +56,7 @@ PROCESS_THREAD(shellGroupP, ev, data)
 		PROCESS_WAIT_EVENT();
 		if (ev == NEW_MODEL_IN_JSON) {
 			/* wow I have a new model, do te magic with the traces and so on */
-			printf("New model received in group\n");
+			printf("New model %s received in group with instance %p\n", instance->fileNameWithModel, instance);
 			/* TODO Paco, you should load the model into a ContainerRoot from the json file with name in variable instance->fileNameWithModel*/
 
 
@@ -62,19 +68,35 @@ PROCESS_THREAD(shellGroupP, ev, data)
 
 				if((cfs_read(fd_read, jsonModel, length + 1)) != -1) {
 					printf("INFO: new_model JSON loaded in RAM\n");
-					/*printf("%s\n", jsonModel);*/
 				} else {
 					printf("ERROR: Empty model!\n");
 				}
 
+				cfs_close(fd_read);
+
 				struct jsonparse_state jsonState;
 
 				jsonparse_setup(&jsonState, jsonModel, strlen(jsonModel) + 1);
-				newModel = JSONKevDeserializer(&jsonState, jsonparse_next(&jsonState));
+				intiModel = JSONKevDeserializer(&jsonState, jsonparse_next(&jsonState));
+				printf("INFO: Deserialized finished\n");
+				free(jsonModel);
+				/*Visitor *visitor = malloc(sizeof(Visitor));
+				visitor->action = actionprintf;
+				visitor->secondAction = NULL;
 
+				intiModel->Visit(intiModel, visitor);*/
 			}
 			/* TODO Afterwards, just call notifyNewModel */
-			notifyNewModel(newModel);
+			if (intiModel != NULL) {
+				int err;
+				if ((err = notifyNewModel(intiModel)) == PROCESS_ERR_OK) {
+					printf("INFO: Model was successfully sent\n");
+				} else {
+					printf("ERROR: Something is wrong sending the model, err: %d\n", err);
+				}
+			} else {
+				printf("ERROR: The model cannot be loaded!\n");
+			}
 		}
 		else if (ev == PROCESS_EVENT_EXITED || ev == PROCESS_EVENT_EXIT) {
 			PROCESS_EXIT();
@@ -100,7 +122,11 @@ int startShellGroup(void* instance)
 
 	inst->fileNameWithModel = "new_model-compact.json";
 
-	process_start(&shellGroupP, instance);
+	printf("INFO: Sending instance %s, at %p\n", inst->fileNameWithModel, inst);
+
+	process_start(&shellGroupP, inst);
+
+	printf("INFO: Sent instance %s, at %p\n", inst->fileNameWithModel, inst);
 
 	return 0;
 }
