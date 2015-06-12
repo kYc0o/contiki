a648c01460d636fbf3cc34c31a6b14b694d507d0
Alexandre Rio <alexandre.rio@irisa.fr>
2015-06-12 18:11:16 +0200

    try to disseminate not compressed model

--- examples/compress/compress_shell.c
+++ examples/compress/compress_shell.c
@@ -1,9 +1,8 @@
 #include "contiki.h"
-//#include "dev/serial-line.h"
+#include "dev/serial-line.h"
 #include "sys/node-id.h"
-//#include "sys/etimer.h"
 #include <stdbool.h>
-//#include "deluge-udp.h"
+#include "deluge-udp.h"
 #include <stdio.h>
 
 #include "cfs/cfs-coffee.h"
@@ -17,88 +16,94 @@
 #endif
 
 #ifndef COMPRESSED_FILE_SIZE
-#define COMPRESSED_FILE_SIZE 1635
+#define COMPRESSED_FILE_SIZE 1713
 #endif
 
 static const char *DEFAULTMODEL = "{\"eClass\" : \"org.kevoree.ContainerRoot\",\"generated_KMF_ID\" : \"BXX5q3eV\",\"nodes\" : [{\"eClass\" : \"org.kevoree.ContainerNode\",\"name\" : \"node0\",\"metaData\" : \"\",\"started\" : \"1\",\"components\" : [],\"hosts\" : [],\"host\" : [],\"groups\" : [\"groups[group0]\"],\"networkInformation\" : [{\"eClass\" : \"org.kevoree.NetworkInfo\",\"name\" : \"ip\",\"values\" : [{\"eClass\" : \"org.kevoree.NetworkProperty\",\"name\" : \"front\",\"value\" : \"m3-XX.lille.iotlab.info\"},{\"eClass\" : \"org.kevoree.NetworkProperty\",\"name\" : \"local\",\"value\" : \"fe80:0000:0000:0000:0323:4501:4471:0343\"}]}],\"typeDefinition\" : [\"typeDefinitions[ContikiNode/0.0.1]\"],\"dictionary\" : [],\"fragmentDictionary\" : []}],\"typeDefinitions\" : [{\"eClass\" : \"org.kevoree.NodeType\",\"name\" : \"ContikiNode\",\"version\" : \"0.0.1\",\"factoryBean\" : \"\",\"bean\" : \"\",\"abstract\" : \"0\",\"deployUnit\" : [\"deployUnits[org.kevoree.library.c//kevoree-contiki-node/0.0.1]\"],\"dictionaryType\" : [{\"eClass\" : \"org.kevoree.DictionaryType\",\"generated_KMF_ID\" : \"o8AVQY3e\",\"attributes\" : []}],\"superTypes\" : []},{\"eClass\" : \"org.kevoree.GroupType\",\"name\" : \"CoAPGroup\",\"version\" : \"0.0.1\",\"factoryBean\" : \"\",\"bean\" : \"\",\"abstract\" : \"0\",\"deployUnit\" : [\"deployUnits[//kevoree-group-coap/0.0.1]\"],\"dictionaryType\" : [{\"eClass\" : \"org.kevoree.DictionaryType\",\"generated_KMF_ID\" : \"3dddTFpd\",\"attributes\" : [{\"eClass\" : \"org.kevoree.DictionaryAttribute\",\"name\" : \"proxy_port\",\"optional\" : \"1\",\"state\" : \"0\",\"datatype\" : \"int\",\"fragmentDependant\" : \"1\",\"defaultValue\" : \"20000\"},{\"eClass\" : \"org.kevoree.DictionaryAttribute\",\"name\" : \"port\",\"optional\" : \"1\",\"state\" : \"0\",\"datatype\" : \"number\",\"fragmentDependant\" : \"1\",\"defaultValue\" : \"\"},{\"eClass\" : \"org.kevoree.DictionaryAttribute\",\"name\" : \"path\",\"optional\" : \"1\",\"state\" : \"0\",\"datatype\" : \"string\",\"fragmentDependant\" : \"1\",\"defaultValue\" : \"\"}]}],\"superTypes\" : []}],\"repositories\" : [],\"dataTypes\" : [],\"libraries\" : [{\"eClass\" : \"org.kevoree.TypeLibrary\",\"name\" : \"ContikiLib\",\"subTypes\" : [\"typeDefinitions[ContikiNode/0.0.1]\",\"typeDefinitions[CoAPGroup/0.0.1]\"]},{\"eClass\" : \"org.kevoree.TypeLibrary\",\"name\" : \"Default\",\"subTypes\" : []}],\"hubs\" : [],\"mBindings\" : [],\"deployUnits\" : [{\"eClass\" : \"org.kevoree.DeployUnit\",\"name\" : \"kevoree-group-coap\",\"groupName\" : \"\",\"version\" : \"0.0.1\",\"url\" : \"\",\"hashcode\" : \"\",\"type\" : \"ce\"},{\"eClass\" : \"org.kevoree.DeployUnit\",\"name\" : \"kevoree-contiki-node\",\"groupName\" : \"org.kevoree.library.c\",\"version\" : \"0.0.1\",\"url\" : \"\",\"hashcode\" : \"\",\"type\" : \"ce\"}],\"nodeNetworks\" : [],\"groups\" : [{\"eClass\" : \"org.kevoree.Group\",\"name\" : \"group0\",\"metaData\" : \"\",\"started\" : \"1\",\"subNodes\" : [\"nodes[node0]\"],\"typeDefinition\" : [\"typeDefinitions[CoAPGroup/0.0.1]\"],\"dictionary\" : [],\"fragmentDictionary\" : [{\"eClass\" : \"org.kevoree.FragmentDictionary\",\"generated_KMF_ID\" : \"VEj2RlNr\",\"name\" : \"contiki-node\",\"values\" : []}]}]}";
 
 unsigned short node_id = 1;
 
-PROCESS(compression, "Model compression");
+PROCESS(compression, "kev-compress");
 AUTOSTART_PROCESSES(&compression);
 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(compression, ev, data)
 {
+  static uint8_t buf[257];
   char model_buffer[PLAIN_FILE_SIZE];
-  static struct etimer et;
+  static struct cfs_dirent dirent;
+  static struct cfs_dir dir;
+  static uint32_t fdFile;
   static char *filename;
-  char * f_name = "model.json";
+  char* in;
+  char* out;
+  char * f_name = "m";
   int fd, s;
   PROCESS_BEGIN();
 
-  printf("node_id: %d\n", node_id);
-
-
-  memset(model_buffer, 0, sizeof(model_buffer));
-
-  if(node_id == SINK_ID) {
-    memset(model_buffer, 0, sizeof(model_buffer));
-    strcpy(model_buffer, DEFAULTMODEL);
-  } else {
-    memset(model_buffer, 'x', sizeof(model_buffer));
-  }
-
-  cfs_remove(f_name);
-  fd = cfs_open(f_name, CFS_WRITE);
-  if(fd < 0) {
-    printf("going to exit now\n");
-    process_exit(NULL);
-  }
-  if((s = cfs_write(fd, model_buffer, sizeof(model_buffer))) != sizeof(model_buffer)) {
-    cfs_close(fd);
-    printf("ERROR: File written incorrectly\n");
-    process_exit(NULL);
-  } else {
-    printf("Written %d bytes\n", s);
-  }
-
-  cfs_close(fd);
-  if (!deluge_disseminate(f_name, 1, NULL))
-  {
-    printf("Disseminating %s\n", f_name);
-  } else {
-    printf("Error disseminating\n");
-  }
-
-  etimer_set(&et, CLOCK_SECOND*5);
-
   while(1) {
-    printf("waiting\n");
-    PROCESS_WAIT_EVENT();
-    printf("doing stuff now\n");
-
-    if (ev == PROCESS_EVENT_TIMER) {
-      if(node_id != SINK_ID) {
-        fd = cfs_open(f_name, CFS_READ);
-        if(fd < 0) {
-          printf("failed to open %s file\n", f_name);
-        } else {
-          s = cfs_read(fd, model_buffer, sizeof(model_buffer));
-          model_buffer[sizeof(model_buffer) - 1] = '\0';
-          if(s <= 0) {
-            printf("failed to read data from the file\n");
-          } else {
-            printf("File contents: \n%s\n", model_buffer);
-          }
-          cfs_close(fd);
-        }
-      } else {
-        printf("still disseminating...\n");
+    PROCESS_YIELD();
+    if (ev == serial_line_event_message) {
+      if (!strcmp(data, "ls")) {
+	if(cfs_opendir(&dir, ".") == 0) {
+	  while(cfs_readdir(&dir, &dirent) != -1) {
+	    printf("File: %s (%ld bytes)\n",
+		dirent.name, (long)dirent.size);
+	  }
+	  cfs_closedir(&dir);
+	}
       }
-      etimer_restart(&et);
-    }
+      else if (strstr(data, "cat") == data) {
+	int n, jj;
+	char* tmp = strstr(data, " ");
+	tmp++;
+	fdFile = cfs_open(tmp, CFS_READ);
+	if (fdFile < 0) printf("error opening the file %s\n", tmp);
+	while ((n = cfs_read(fdFile, buf, 60)) > 0) {
+	  for (jj = 0 ; jj < n ; jj++) printf("%c", (char)buf[jj]);
+	}
+	printf("\n");
+	cfs_close(fdFile);
+	if (n!=0)
+	  printf("Some error reading the file\n");
 
+      }
+      else if (strstr(data, "rm") == data) {
+	char* tmp = strstr(data, " ");
+	tmp++;
+	cfs_remove(tmp);
+      }
+      else if (strstr(data, "format") == data) {
+	printf("Formatting\n");
+	printf("It takes around 3 minutes\n");
+	printf("...\n");
+	fdFile = cfs_coffee_format();
+	printf("Formatted with result %ld\n", fdFile);
+      }
+      else if (!strcmp(data, "write")) {
+	int fd_model = cfs_open("m", CFS_WRITE);
+	cfs_write(fd_model, DEFAULTMODEL, strlen(DEFAULTMODEL));
+	cfs_close(fd_model);
+      }
+      else if (strstr(data, "compress") == data) {
+	in = "m";
+	out = "c";
+	printf("Compressing '%s' to '%s'\n", in, out);
+	compress(in, out);
+	printf("Done compressing!\n");
+      }
+      else if (strstr(data, "decompress") == data) {
+	in = "c";
+	out = "d";
+	printf("Decompressing '%s' to '%s'\n", in, out);
+	decompress(in, out);
+	printf("Done decompressing!\n");
+      }
+      else
+      {
+	printf("Unknown command.\n");
+      }
+    }
   }
   PROCESS_END();
 }
