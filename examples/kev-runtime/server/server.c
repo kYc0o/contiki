179894d567471dd1f256488be895788e7494998d
kYc0o <fco.ja.ac@gmail.com>
2015-07-06 20:19:36 +0200

    server almost working

--- examples/kev-runtime/server/server.c
+++ examples/kev-runtime/server/server.c
@@ -15,6 +15,7 @@
 #include <errno.h>
 
 #include "requests.h"
+#include "uip.h"
 
 #include "lib/list.h"
 
@@ -34,12 +35,12 @@ fill_repository()
 	size_t len = 0;
 	ssize_t read;
 	char path[255];
-	
+
 	sprintf(path, "%s/repository.repo", reposotory_base_path);
 
 	fp = fopen(path, "r");
 	if (fp == NULL)
-	   exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 
 	while ((read = getline(&line, &len, fp)) != -1) {
 		char* tmp = strstr(line, "\n");
@@ -47,10 +48,10 @@ fill_repository()
 		tmp = strstr(line, " ");
 		*tmp = 0;
 		tmp++;
-		
+
 		if (find_by_artifact(art_to_file, line) == NULL) {
-			
-		
+
+
 			struct MapEntry* entry = (struct MapEntry*)malloc(sizeof(struct MapEntry));
 			sprintf(path, "%s/%s", reposotory_base_path, tmp);
 			entry->artifact = strdup(line);
@@ -61,22 +62,26 @@ fill_repository()
 	}
 
 	free(line);
-	
+
 	fclose(fp);
 }
 
 static void server_onNewSummary(uint16_t session_id, uint16_t nr_chunks);
 static void server_onNewChunk(uint16_t chunk_id, uint16_t len, const uint8_t* data);
 static void server_onAckArtifactRequest(void);
-static void server_onArtifactRequest(const char* source_address, const char* artifact);
+static void server_onArtifactRequest(const uip_ipaddr_t* source_address, const char* artifact);
 static void server_onChunkRequest(uint16_t session_id, uint16_t chunk_id);
+static void server_onRepoAddrRequest(uip_ipaddr_t *src, uip_ipaddr_t *reqSrc, char *duName);
+static void server_onRepoAddr(uip_ipaddr_t *src);
 
 static const struct RequestProcessingCallback server_request_callbacks = {
-	.onNewSummary = server_onNewSummary,
-	.onNewChunk = server_onNewChunk,
-	.onAckArtifactRequest = server_onAckArtifactRequest,
-	.onArtifactRequest = server_onArtifactRequest,
-	.onChunkRequest = server_onChunkRequest
+		.onNewSummary = server_onNewSummary,
+		.onNewChunk = server_onNewChunk,
+		.onAckArtifactRequest = server_onAckArtifactRequest,
+		.onArtifactRequest = server_onArtifactRequest,
+		.onChunkRequest = server_onChunkRequest,
+		.onRepoRequest = server_onRepoAddrRequest,
+		.onRepoAddr = server_onRepoAddr
 };
 
 static void
@@ -115,47 +120,48 @@ prepare_request_with_artifact(struct DeployUnitRequest* req, const char* artifac
 	req->current_packet = 0;
 	req->nr_packets = s / REQUEST_PACKET_SIZE;
 	if (s % REQUEST_PACKET_SIZE) req->nr_packets++;
-	
+
 }
 
 static void
-server_onArtifactRequest(const char* source_address, const char* artifact)
+server_onArtifactRequest(const uip_ipaddr_t* source_address, const char* artifact)
 {
-	printf("Address '%s', Artifact: '%s'\n", source_address, artifact);
-	
-	
-	struct DeployUnitRequest* req = find_request_by_source(requests_as_server, source_address, artifact);
-	
+	char *srcAddr = convertAddr(source_address);
+	printf("Address '%s', Artifact: '%s'\n", srcAddr, artifact);
+
+
+	struct DeployUnitRequest* req = find_request_by_source(requests_as_server, srcAddr, artifact);
+
 	int is_new = req == NULL;
-	
+
 	if (req == NULL) {
 		/* a new request */
-		req = create_request(source_address, artifact, SENDING_SUMMARY);
+		req = create_request(srcAddr, artifact, SENDING_SUMMARY);
 		list_add(requests_as_server, req);
 	}
-	
+
 	/* requesting the same */
 	if (req->state == SENDING_SUMMARY || req->state == WAITING_FOR_LOCATION) {
 		/* send summary to the source */
-		
+
 		if (is_artifact_location_known(req)) {
 			printf("\tSending summary\n");
 			/* filling the information about the packet */
 			prepare_request_with_artifact(req, artifact);
 			struct KevoreePacket pkt;
 			build_summary_packet(&pkt, req->session_id, req->nr_packets);
-			
+
 			/* send response back */
 			struct sockaddr_in6 cliaddr;
 			cliaddr.sin6_family = AF_INET6;
-			inet_pton(AF_INET6, source_address, &cliaddr.sin6_addr);
+			inet_pton(AF_INET6, srcAddr, &cliaddr.sin6_addr);
 			cliaddr.sin6_port=htons(PORT);
 			sendto(sockfd, &pkt, total_len(&pkt), 0, (struct sockaddr *)&cliaddr,sizeof(cliaddr));
 		}
 		else {
-		
+
 			printf("Wrong <=======\n");
-		
+
 			while(1) {  };
 		}
 	}
@@ -164,26 +170,27 @@ static void
 server_onChunkRequest(uint16_t session_id, uint16_t chunk_id)
 {
 	struct DeployUnitRequest* req = find_request_by_session(requests_as_server, session_id);
-	
+	char _reqSrc[16];
+
 	if (req != NULL && 
 			((req->state == SENDING_CHUNKS && req->current_packet <= chunk_id) ||
-			(req->state == SENDING_SUMMARY && req->current_packet == 0 && chunk_id == 0))  ) {
-	
+					(req->state == SENDING_SUMMARY && req->current_packet == 0 && chunk_id == 0))  ) {
+
 		req->current_packet = chunk_id;
-		
+
 		/* modifying the state */
 		req->state = SENDING_CHUNKS;
-	
+
 		printf("Session Id '%d', Chunk Id: '%d'. %d %d '%s'\n", session_id, chunk_id, req->state, req->current_packet, req->source_address);
 		/* read from the file */
 		uint8_t buf [REQUEST_PACKET_SIZE];
 		lseek(req->fd, chunk_id * REQUEST_PACKET_SIZE, SEEK_SET);
 		int n = read(req->fd, buf, REQUEST_PACKET_SIZE);
-		
+
 		/* build the packet */
 		struct KevoreePacket pkt;
 		build_chunk_packet(&pkt, chunk_id, n, buf);
-		
+
 		/* send response back */
 		struct sockaddr_in6 cliaddr;
 		cliaddr.sin6_family = AF_INET6;
@@ -193,16 +200,37 @@ server_onChunkRequest(uint16_t session_id, uint16_t chunk_id)
 	}
 }
 
+static void
+server_onRepoAddrRequest(uip_ipaddr_t *src, uip_ipaddr_t *reqSrc, char *duName)
+{
+	struct KevoreePacket pkt;
+	build_repo_addr(&pkt, duName);
+	struct sockaddr_in6 cliaddr;
+	cliaddr.sin6_family = AF_INET6;
+	printf("Receiving request from: %s\n", convertAddr(reqSrc));
+	inet_pton(AF_INET6, convertAddr(reqSrc), &cliaddr.sin6_addr);
+	cliaddr.sin6_port=htons(PORT);
+	sendto(sockfd, &pkt, total_len(&pkt), 0, (struct sockaddr *)&cliaddr,sizeof(cliaddr));
+}
+
+static void
+server_onRepoAddr(uip_ipaddr_t *src)
+{
+	printf("ERROR: Everything is wrong from %s at %s:%d\n", convertAddr(src), __FILE__, __LINE__);
+}
+
+
 int
 main (int argc, char *argv[])
 {
+	uip_ipaddr_t ip;
 	list_init(art_to_file);
 	fill_repository();
-	
+
 	list_init(requests_as_server);
 
 	printf("Server started with size: %d\n", sizeof(struct KevoreePacket));
-	
+
 	int n;
 	struct sockaddr_in6 servaddr;
 	struct sockaddr_in6 cliaddr;
@@ -211,7 +239,7 @@ main (int argc, char *argv[])
 	char address_name[80];
 
 	sockfd = socket(AF_INET6,SOCK_DGRAM,0);
-	
+
 	if (sockfd < 0)  {
 		printf("ERROR: no socket available\n");
 		return -1;
@@ -229,13 +257,15 @@ main (int argc, char *argv[])
 	/* loop for ever */
 	for (;;)
 	{
-	  len = sizeof(cliaddr);
-	  n = recvfrom(sockfd,(char*)mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
-	  inet_ntop(AF_INET6, (struct sockaddr *)&cliaddr.sin6_addr, address_name, 80);
-	  enum MessageProcessingErroCode err = process_message(address_name, n, mesg, &server_request_callbacks);
-	  if (err != NONE) {
-	  	printf("Error %d processing message\n", err);
-	  }
+		len = sizeof(cliaddr);
+		n = recvfrom(sockfd,(char*)mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
+		inet_ntop(AF_INET6, (struct sockaddr *)&cliaddr.sin6_addr, address_name, 80);
+		sscanf(address_name, "%02x%02x::%02x%02x", &(ip.u8[0]), &(ip.u8[1]), &(ip.u8[14]), &(ip.u8[15]));
+		printf("Processing message from: %s\n", convertAddr(&ip));
+		enum MessageProcessingErroCode err = process_message(&ip, n, mesg, &server_request_callbacks);
+		if (err != NONE) {
+			printf("Error %d processing message\n", err);
+		}
 	}
 
 	return 0;
